088a8804f411c947fe98987f1b9bea34f219ef1e
adhamkhaled312 <adhamkhaled312@gmail.com>
2024-02-03 12:27:22 +0200

    added documentation

--- Tasks/Task3/mypwd.c
+++ Tasks/Task3/mypwd.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define MAX_SIZE 100
+#define MAX_SIZE 200
 #define RET_STAT 0
 int main()
 {
+    //Define string to save the pathname 
     unsigned char buf[MAX_SIZE];
+
+    //define string for the return value
+    //in case of failure will contain NULL
     unsigned char *ret;
+
+    //get Pathname using getcwd
     ret= getcwd(buf,MAX_SIZE);
+
+    //if NULL there is an error in getting pathname (buffer size should increase)
     if(!ret){
         printf("Couldn't get pathname\n");
     }
+    //if not NULL then print the pathname
     else{
     	printf("%s\n",buf);
     }
