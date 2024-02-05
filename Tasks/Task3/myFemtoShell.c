08c03219c821cca150ef48b101b24bd687f27688
adhamkhaled312 <adhamkhaled312@gmail.com>
2024-02-05 14:38:57 +0200

    add separate function for parsing

--- Tasks/Task3/myFemtoShell.c
+++ Tasks/Task3/myFemtoShell.c
@@ -4,7 +4,10 @@
 
 // Define a macro constant representing the maximum length of input string
 #define MAX 100
+// Define a macro for maximum number of arguments
+#define ARG_MAX 20
 
+void parse(char *input, char *args[]);
 int main()
 {
     // Declare a character array to store user input
@@ -20,15 +23,14 @@ int main()
 	// Remove the trailing newline character
 	input[strcspn(input, "\n")] = '\0';
 	// Handling the input as newline
-	if(0==strlen(input)){
+	if (0 == strlen(input)) {
 	    printf("Please enter valid command\n");
 	    continue;
 	}
-	char *token;		//to save current token
+	char *args[ARG_MAX];	// array to save arguments of the command 
+	parse(input, args);	// parsing the command
 	char ret;		// to check for the success of operations
-	token = strtok(input, " ");
-	//check if current command is pwd
-	if (!strcmp(token, "pwd")) {
+	if (!strcmp(args[0], "pwd")) {
 	    //Define string to save the pathname 
 	    unsigned char buf[200];
 	    //define string for the return value
@@ -45,29 +47,25 @@ int main()
 		printf("%s\n", buf);
 	    }
 	    // check if current command is cd
-	} else if (!strcmp("cd", token)) {
-	    //get the path
-	    token = strtok(NULL, " ");
+	} else if (!strcmp("cd", args[0])) {
 	    //go to the path using chdir
-	    ret = chdir(token);
+	    ret = chdir(args[1]);
 	    //check for successful changing 
 	    if (ret != 0) {
 		printf("Cannot change directory\n");
 	    }
 	    // check if current command is echo
-	} else if (!strcmp("echo", token)) {
-	    //print or upcoming tokens (strings), only print if not null
-	    while (token != NULL) {
-		token = strtok(NULL, " ");
-		if (token) {
-		    printf("%s ", token);
-		}
+	} else if (!strcmp("echo", args[0])) {
+	    int i = 1;
+	    // Print the arguments starting from args[1] separated by space
+	    for (i; args[i] != NULL; i++) {
+		printf("%s ", args[i]);
 	    }
-	    //print new line after finishing 
+	    // Print new line after finishing
 	    printf("\n");
 	}
 	//check if command is exit, get out of the shell
-	else if (!strcmp("exit", token)) {
+	else if (!strcmp("exit", args[0])) {
 	    printf("Good Bye :)\n");
 	    break;
 	    //if the command is not supported print error message
@@ -77,4 +75,20 @@ int main()
     }
 
     return 0;
+}
+
+/**
+ * Function to parse the command into arguments
+ * input: the command input string
+ * args: array of strings to save the arguments
+ */
+void parse(char *input, char *args[])
+{
+    int i = 0;
+    args[i] = strtok(input, " ");
+    while (i < ARG_MAX && args[i] != NULL) {
+	i++;
+	args[i] = strtok(NULL, " ");
+    }
+    return;
 }
