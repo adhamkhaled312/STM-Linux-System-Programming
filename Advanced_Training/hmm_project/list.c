1be8b61642ded21c8c59cf53f959751e3810976b
adhamkhaled312 <adhamkhaled312@gmail.com>
2024-04-17 15:19:39 +0200

    modify return types

--- Advanced_Training/hmm_project/list.c
+++ Advanced_Training/hmm_project/list.c
@@ -11,21 +11,23 @@ static char moveBrkDown(block_t *block,size_t decSize);
  * 
  * @param list pointer to the first entry of the list
  * @param size the size to be allocated at the beginning
- * @return char the return status -1:error 0:no error
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOT_OK): The function had issue to perform this action  
  */
-char init(block_t **list,size_t size){
-    char retVal=0;
+Std_ReturnType init(block_t **list,size_t size){
+    Std_ReturnType retVal=E_OK;
     (*list)=(void *)sbrk(size);
     if((void*)-1==list){
         perror("Can't initialize the heap");
-        retVal=-1;
+        retVal=E_NOT_OK;
     }
     else{
         (*list)->next=NULL;
         (*list)->prev=NULL;
         (*list)->size=(size-sizeof(block_t));
         (*list)->status=FREE_BLOCK;
-        retVal=0;
+        retVal=E_OK;
     }
     return retVal;
 }
@@ -34,7 +36,7 @@ char init(block_t **list,size_t size){
  * @brief used to split a block of data into two blocks
  *        it's used in case of finding a block which is bigger than block needed
  * @param fitting pointer to the block which will be split
- * @param size 
+ * @param size the size of the block after splitting
  */
 void split(block_t *fitting,size_t size){
     //if the remaining part will not be enough for metadata then don't split and mark block as used
@@ -61,21 +63,23 @@ void split(block_t *fitting,size_t size){
  *        first check if the block is free then extend it, if used then allocate new space
  * @param list pointer to the last block in the current free list
  * @param size the size to be allocated
- * @return char the return status -1:error 0:no error
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOT_OK): The function had issue to perform this action  
  */
-char new_alloc(block_t *list,size_t size){
-    char retVal=0;
+Std_ReturnType new_alloc(block_t *list,size_t size){
+    Std_ReturnType retVal=E_OK;
     //if the last block in the list is free then extend it so it can hold the data
     if(FREE_BLOCK==list->status){
         void*temp=sbrk(size);
         if((void *)-1==temp){
             perror("Can't allocate new space");
-            retVal=-1;
+            retVal=E_NOT_OK;
     }
         else{
             //the size of the block will be the current size + the extended space size
             list->size+=size;
-            retVal=0;
+            retVal=E_OK;
     }
     }
     else{
@@ -83,7 +87,7 @@ char new_alloc(block_t *list,size_t size){
         block_t *new=(void*)sbrk(size);
         if((void*)-1==new){
             perror("Can't allocate new space");
-            retVal=-1;
+            retVal=E_NOT_OK;
         }
         else{
             //configure new block metadata and point from the current block to it
@@ -92,7 +96,7 @@ char new_alloc(block_t *list,size_t size){
             new->next=NULL;
             new->status=FREE_BLOCK;
             list->next=new;
-            retVal=0;
+            retVal=E_OK;
         }
     }
     return retVal;
@@ -102,10 +106,10 @@ char new_alloc(block_t *list,size_t size){
  *        it's also responsible for decreasing the program break if that can be done 
  * @param block pointer to the block to check on
  * @param decSize the minimum size of the free block needed to decrease the program break
- * @return char the return status -1:error 0:no error
- */
-char merge(block_t* block,size_t decSize){
-    char retVal=0;
+*/
+
+void merge(block_t* block,size_t decSize){
+    Std_ReturnType retVal=0;
     //if the next block is also free block merge both blocks
     if(NULL!=(block->next) && FREE_BLOCK==(block->next)->status){
         //size of current block increased by size of the next block and its metadata
@@ -134,44 +138,53 @@ char merge(block_t* block,size_t decSize){
     //then decrease sbrk to release the space to the kernel
     if(NULL==block->next && FREE_BLOCK==block->status && block->size>=decSize){
         retVal=moveBrkDown(block,decSize);
+        if(E_NOT_OK==retVal){
+            perror("Can't decrease program break");
+        }
     }
-    return retVal;
 }
 /**
  * @brief helper function to decrease the program break
  * 
-*/
-static char moveBrkDown(block_t *block,size_t decSize){
+ * @param block pointer to the block to check on
+ * @param decSize the minimum size of the free block needed to decrease the program break
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOT_OK): The function had issue to perform this action  
+ */
+static Std_ReturnType moveBrkDown(block_t *block,size_t decSize){
     void *temp;
-    char retVal=0;
+    Std_ReturnType retVal=0;
     //move sbrk down with sufficient multiple of decSize 
     // in order not to call sbrk on the next free
     // and also leave some space (if that can be done) so next malloc may not call sbrk
     size_t remaining=block->size-((block->size)/decSize)*decSize;
+    size_t decValue=((block->size)/decSize)*decSize;
     //if the block size is exactly size multiple of decSize
     //remove the last block
     if(0==remaining){
         block->prev->next=NULL;
-        temp=sbrk(-((((block->size)/decSize)*decSize)+sizeof(block_t)));
+        temp=sbrk(-((decValue)+sizeof(block_t)));
         if((void *)-1==temp){
-            perror("Can't decrease program break");
-            retVal=-1;
+            retVal=E_NOT_OK;
         }
         else{
-            retVal=0;
+            retVal=E_OK;
         }
     }
     //if the remaining size isn't 0 
     //just change the size of last block
     else{
+        //printf("\n%p    %p    %li    %li",block,sbrk(0),block->size,decValue);
         block->size=remaining;
-        temp=sbrk(-(((block->size)/decSize)*decSize));
+        //getchar();
+       // printf("    %li\n",block->size);
+        temp=sbrk(-(decValue));
             if((void *)-1==temp){
-            perror("Can't decrease program break");
-            retVal=-1;
+            retVal=E_NOT_OK;
         }
         else{
-            retVal=0;
+            retVal=E_OK;
         }
     }
     
