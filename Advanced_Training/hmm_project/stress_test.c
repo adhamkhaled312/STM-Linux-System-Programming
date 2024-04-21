a71b272c0faf07cba70e5643698802662779577f
adhamkhaled312 <adhamkhaled312@gmail.com>
2024-04-21 15:56:47 +0200

    add stress test

---
+++ Advanced_Training/hmm_project/stress_test.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "hmm.h"
+
+void * malloc(size_t size)
+{
+    return HmmAlloc(size);
+}
+
+void free(void *ptr)
+{
+    HmmFree(ptr);
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+	return HmmCalloc(nmemb,size);
+}
+
+void *realloc(void *ptr, size_t size)
+{
+	return HmmRealloc(ptr,size);
+}
+
+#define NUM_ALLOCS 1000
+#define MAX_SIZE 10240
+#define MAX_ITERATIONS 1000
+
+void random_alloc_free_test() {
+    srand((unsigned int)time(NULL));
+    
+    void* pointers[NUM_ALLOCS] = {NULL};
+    
+    for (int i = 0; i < MAX_ITERATIONS; ++i) {
+        int index = rand() % NUM_ALLOCS;
+        if (pointers[index] == NULL) {
+            // Allocate memory
+            size_t size = (size_t)(rand() % MAX_SIZE) + 1;
+            pointers[index] = malloc(size);
+            if (pointers[index] != NULL) {
+                printf("Allocated memory of size %zu at address %p\n", size, pointers[index]);
+            } else {
+                fprintf(stderr, "Allocation failed for size %zu\n", size);
+            }
+        } else {
+            // Free memory
+            printf("Freeing memory at address %p\n", pointers[index]);
+            free(pointers[index]);
+            pointers[index] = NULL;
+        }
+    }
+    
+    // Free remaining allocated memory
+    for (int i = 0; i < NUM_ALLOCS; ++i) {
+        if (pointers[i] != NULL) {
+            printf("Freeing remaining memory at address %p\n", pointers[i]);
+            free(pointers[i]);
+            pointers[i] = NULL;
+        }
+    }
+}
+
+int main() {
+    printf("Starting random allocation and deallocation test...\n");
+    random_alloc_free_test();
+    printf("Test complete.\n");
+    return 0;
+}
+
