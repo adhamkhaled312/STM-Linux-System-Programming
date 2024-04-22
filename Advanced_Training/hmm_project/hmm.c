c2121fa8ef37aa680af827fad79faf92bc49ddb8
adhamkhaled312 <adhamkhaled312@gmail.com>
2024-04-23 01:54:02 +0200

    change senario of calling malloc(0)

--- Advanced_Training/hmm_project/hmm.c
+++ Advanced_Training/hmm_project/hmm.c
@@ -20,60 +20,52 @@ void *HmmAlloc(size_t size){
     block_t *curr;
     void *retVal=NULL;
     Std_ReturnType temp;
-    //if size is zero retrn NULL
-    if(0==size){
-        retVal=NULL;
+    // if there is no block assigned (the list is still empty)
+    if(!firstBlock){
+        //initialize the heap by allocating multiple of ALLOC_SPACE pages in it
+        //that handles if the user entered very big size
+        temp=init(&firstBlock,((size/ALLOC_SPACE)+1)*ALLOC_SPACE);
+        //if error occured during allocating set retVal to null
+        if(E_NOT_OK==temp){
+            retVal=NULL;
+        }
+        else{
+            //split the allocated pages into the part user needed and the other part is free
+            split(firstBlock,size);
+            //return to the user pointer to the start of the data (after the metadata)
+            retVal=(void*)((void*)firstBlock+sizeof(block_t));
+        }
     }
     else{
-        
-        // if there is no block assigned (the list is still empty)
-        if(!firstBlock){
-            //initialize the heap by allocating multiple of ALLOC_SPACE pages in it
-            //that handles if the user entered very big size
-            temp=init(&firstBlock,((size/ALLOC_SPACE)+1)*ALLOC_SPACE);
-            //if error occured during allocating set retVal to null
+        // traverse free list to find the suitable block
+        curr=firstBlock;
+        TRAVERSE_LIST(curr,size);
+        //if a block with found the same size allocate it (mark it as not free)
+        if(size+sizeof(block_t)==(curr->size)&& FREE_BLOCK==curr->status){
+            SET_BLOCK_USED(curr);
+            retVal=(void*)((void*)curr+sizeof(block_t));
+        }
+        //if a block found with size greater than needed and has enough extra space for meta data
+        else if((curr->size)>size+sizeof(block_t) && FREE_BLOCK==curr->status){
+            split(curr,size);
+            retVal=(void*)((void*)curr+sizeof(block_t));
+        }
+        //no block is found in the free list then we need to allocate new space in heap
+        else{
+            temp=new_alloc(curr,((size/ALLOC_SPACE)+1)*ALLOC_SPACE);
             if(E_NOT_OK==temp){
                 retVal=NULL;
             }
             else{
-                //split the allocated pages into the part user needed and the other part is free
-                split(firstBlock,size);
-                //return to the user pointer to the start of the data (after the metadata)
-                retVal=(void*)((void*)firstBlock+sizeof(block_t));
-            }
-        }
-        else{
-            // traverse free list to find the suitable block
-            curr=firstBlock;
-            TRAVERSE_LIST(curr,size);
-            //if a block with found the same size allocate it (mark it as not free)
-            if(size+sizeof(block_t)==(curr->size)&& FREE_BLOCK==curr->status){
-                SET_BLOCK_USED(curr);
-                retVal=(void*)((void*)curr+sizeof(block_t));
-            }
-            //if a block found with size greater than needed and has enough extra space for meta data
-            else if((curr->size)>size+sizeof(block_t) && FREE_BLOCK==curr->status){
+                //if the current block is used then a new block is allocated
+                if(USED_BLOCK==curr->status){
+                    curr=curr->next;
+                }
                 split(curr,size);
-                retVal=(void*)((void*)curr+sizeof(block_t));
-            }
-            //no block is found in the free list then we need to allocate new space in heap
-            else{
-                temp=new_alloc(curr,((size/ALLOC_SPACE)+1)*ALLOC_SPACE);
-                if(E_NOT_OK==temp){
-                    retVal=NULL;
-                }
-                else{
-                    //if the current block is used then a new block is allocated
-                    if(USED_BLOCK==curr->status){
-                        curr=curr->next;
-                    }
-                    split(curr,size);
-                    retVal=(void *)((void*)curr+sizeof(block_t));
-                }
+                retVal=(void *)((void*)curr+sizeof(block_t));
             }
         }
     }
-    
     return retVal;
 }
 /**
